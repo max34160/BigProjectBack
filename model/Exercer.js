7bbc410d44ff3420f9b1c914c23244272271518d
CamossiJeremy <camossijeremy@gmail.com>
2026-06-09 16:13:09 +0200

    correction de profil pro et ajouter methodo

--- model/Exercer.js
+++ model/Exercer.js
@@ -6,6 +6,14 @@ export class Exercer extends AbstractModel {
     table = "Exercer";
     colones = ["id_pro", "id_methodo"];
 
+    async create(data) {
+        await db.insert(
+            'INSERT INTO Exercer(id_pro, id_methodo) VALUES (:id_pro, :id_methodo)',
+            data
+        );
+        return data;
+    }
+
     async removeEntry(id_pro, id_methodo) {
         return await db.delete('DELETE FROM Exercer WHERE id_pro = ? AND id_methodo = ?', [id_pro, id_methodo]);
     }
