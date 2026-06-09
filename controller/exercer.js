4065da8cef640c4ae25550641ddecda861e6bc7a
max34 <maximegaussen34160@gmail.com>
2026-06-09 11:43:54 +0200

    Creation et mise en fonction de exercer

---
+++ controller/exercer.js
@@ -0,0 +1,42 @@
+import { Exercer } from "../model/Exercer.js";
+
+
+const model = new Exercer();
+
+export async function getOneByProAndMethodo(req, res) {
+    const data = {
+        id_user = req.params.id_pro,
+        id_methodo = req.params.id_methodo
+    }
+    const exercer = await model.getBy(data);
+    if (exercer) {
+        res.json({exercer : exercer});
+    } else {
+        res.status(404).json({ error: "Pro not founnd"});
+    }
+}
+
+export async function getOneByPro(req, res) {
+    const exercer = await model.get(req.params.id_pro);
+    if (exercer) {
+        res.json({exercer : exercer});
+    } else {
+        res.status(404).json({ error: "Pro not founnd"});
+    }
+}
+
+export async function update(req, res) {
+    req.body.id = req.params.id;
+    const newExercer = await model.update(req.body,req.params.id);
+    res.json({exercer : newExercer});
+}
+
+export async function create(req, res) {
+    const exercer = {
+        id_user : req.body.id_user,
+        id_methodo : req.body.id_methodo
+
+    }
+    const newExercer = await model.create(exercer);
+    res.json(({exercer : newExercer}));
+}
