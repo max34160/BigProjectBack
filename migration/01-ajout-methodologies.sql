23474f5d82c6bb4515168c2a3650b30073e7df75
max34 <maximegaussen34160@gmail.com>
2026-06-12 14:07:53 +0200

    final correct 01 ajout methodologie

--- migration/01-ajout-methodologies.sql
+++ migration/01-ajout-methodologies.sql
@@ -1,16 +1,28 @@
 USE BigProject;
 
 INSERT INTO Methodologie (titre, descriptif, img_presentation) VALUES
-('Médecin', 'Professionnel de santé diplômé en médecine générale ou spécialisé, habilité à diagnostiquer et traiter les maladies.', "images/Médecin.png"),
-('Pharmacien', 'Expert du médicament, il délivre les ordonnances, conseille sur les traitements et veille à la sécurité thérapeutique.', "images/Pharmacien.png"),
-('Chirurgien-Dentiste', 'Spécialiste de la santé bucco-dentaire, il assure la prévention, le diagnostic et le traitement des pathologies dentaires.', "images/Chirurgien-Dentiste.png"),
-('Sage-Femme', 'Professionnelle de santé spécialisée dans le suivi de la grossesse, l\'accouchement et la santé des femmes.', "images/Sage-Femme.png"),
-('Infirmier', 'Professionnel de soins qui assure la prévention, l\'évaluation et le traitement des réponses humaines aux problèmes de santé.', "images/Infirmier.png"),
-('Masseur-Kinésithérapeute', 'Spécialiste de la rééducation fonctionnelle par le mouvement, le massage et les techniques manuelles et physiques.', "images/Masseur-Kinésithérapeute.png"),
-('Pédicure-Podologue', 'Professionnel de santé spécialisé dans le diagnostic et le traitement des affections du pied.', "images/Pédicure-Podologue.png"),
-('Technicien de Laboratoire', 'Expert en analyses biologiques, il réalise les examens de biologie médicale contribuant au diagnostic et au suivi des patients.',"images/Technicien de Laboratoire.png"),
-('Orthophoniste', 'Spécialiste de la rééducation des troubles de la voix, de la parole, du langage oral et écrit.', "images/Orthophoniste.png"),
-('Psychologue', 'Professionnel de la santé mentale qui évalue et accompagne les personnes dans leurs difficultés psychologiques et émotionnelles.', "images/Psychologue.png"),
-('Ergothérapeute', 'Spécialiste qui aide les personnes à retrouver leur autonomie dans les activités de la vie quotidienne après un handicap ou une maladie.', "images/Ergothérapeute.png"),
-('Diététicien', 'Expert en nutrition qui conseille et accompagne les patients dans l\'adoption d\'une alimentation adaptée à leur état de santé.', "images/Diététicien.png"),
-('Psychomotricien', 'Professionnel qui évalue et traite les troubles du mouvement et du comportement liés au développement psychomoteur.', "images/Technicien de Laboratoire.png");
+('Médecin', 'Professionnel de santé titulaire d’un diplôme en médecine générale ou spécialisée. Il assure la prévention, le diagnostic, le traitement et le suivi des maladies. Il accompagne les patients tout au long de leur parcours de soins et coordonne, si nécessaire, l’intervention d’autres professionnels de santé afin d’assurer une prise en charge globale.', 'images/Médecin.png'),
+
+('Pharmacien', 'Professionnel de santé expert du médicament et de son usage. Il délivre les traitements prescrits, vérifie leur conformité et conseille les patients sur leur bonne utilisation. Il participe également à la prévention, au suivi thérapeutique et à l’éducation à la santé afin de garantir une prise en charge sécurisée.', 'images/Pharmacien.png'),
+
+('Chirurgien-Dentiste', 'Professionnel spécialisé dans la santé bucco-dentaire. Il assure la prévention, le diagnostic et le traitement des maladies affectant les dents, les gencives et l’ensemble de la cavité buccale. Il intervient également dans les soins esthétiques, la réhabilitation fonctionnelle et le maintien de la santé orale.', 'images/Chirurgien-Dentiste.png'),
+
+('Sage-Femme', 'Professionnelle de santé spécialisée dans le suivi médical de la grossesse, l’accompagnement à l’accouchement et le suivi postnatal. Elle assure également des missions de prévention, de conseil et de suivi gynécologique tout au long de la vie des femmes.', 'images/Sage-Femme.png'),
+
+('Infirmier', 'Professionnel de santé chargé de dispenser des soins préventifs, curatifs, éducatifs et relationnels auprès des patients. Il évalue l’état de santé, participe au suivi médical, accompagne les personnes dans leur parcours de soins et contribue à l’amélioration de leur qualité de vie.', 'images/Infirmier.png'),
+
+('Masseur-Kinésithérapeute', 'Professionnel spécialisé dans la rééducation, la réadaptation et le maintien des capacités fonctionnelles. Grâce au mouvement, aux techniques manuelles et aux exercices thérapeutiques, il aide les patients à retrouver leur mobilité, réduire leurs douleurs et améliorer leur autonomie.', 'images/Masseur-Kinésithérapeute.png'),
+
+('Pédicure-Podologue', 'Professionnel de santé spécialisé dans l’évaluation, la prévention et le traitement des affections du pied ainsi que des troubles de la posture et de la marche. Il intervient dans la réalisation de soins, de conseils et d’équipements adaptés afin d’améliorer le confort et la mobilité des patients.', 'images/Pédicure-Podologue.png'),
+
+('Technicien de Laboratoire', 'Professionnel spécialisé dans la réalisation et l’analyse des examens biologiques et médicaux. Il prépare les prélèvements, effectue les analyses selon des protocoles rigoureux et participe à la production de résultats fiables essentiels au diagnostic, au suivi et à la prévention des maladies.', 'images/Technicien de Laboratoire.png'),
+
+('Orthophoniste', 'Professionnel de santé spécialisé dans l’évaluation, le diagnostic et la rééducation des troubles de la communication, du langage oral et écrit, de la voix et de la déglutition. Il accompagne les patients de tous âges afin d’améliorer leurs capacités d’expression et de compréhension.', 'images/Orthophoniste.png'),
+
+('Psychologue', 'Professionnel de la santé mentale spécialisé dans l’évaluation du fonctionnement psychologique, émotionnel et comportemental. Il accompagne les personnes dans la compréhension et la gestion de leurs difficultés personnelles, relationnelles ou professionnelles afin de favoriser leur bien-être.', 'images/Psychologue.png'),
+
+('Ergothérapeute', 'Professionnel de santé qui accompagne les personnes confrontées à une limitation physique, psychique ou cognitive afin de préserver ou restaurer leur autonomie. Il propose des solutions adaptées, des aménagements et des activités thérapeutiques facilitant la vie quotidienne.', 'images/Ergothérapeute.png'),
+
+('Diététicien', 'Professionnel expert en nutrition et alimentation. Il évalue les besoins nutritionnels, élabore des programmes alimentaires personnalisés et accompagne les patients dans l’adoption de comportements alimentaires adaptés à leur état de santé et à leurs objectifs.', 'images/Diététicien.png'),
+
+('Psychomotricien', 'Professionnel de santé spécialisé dans l’évaluation et la prise en charge des troubles psychomoteurs. Il intervient sur les dimensions motrices, émotionnelles et relationnelles afin de favoriser le développement, l’adaptation et le bien-être global de la personne.', 'images/Technicien de Laboratoire.png');
